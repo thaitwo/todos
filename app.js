bddac3ff5bfafe7fafc12c227f923af7a6cc2eab
thaitwo <tmtu90@gmail.com>
2018-12-06 21:08:37 -0800

    Add jQuery and some css

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-var todosContainer = document.getElementById('todos-container');
-var todoInput = document.getElementById('todo-input');
 
-function getInputValue() {
-    var key = event.which || event.keyCode;
+(function(){
+  var ENTER_KEY = 13;
 
-    // If "Enter" button is pressed...
-    if (key === 13) {
-      var value = event.target.value;
-      var todo = '<div>' + value + '</div>';
+  var App = {
+    init: function() {
+      this.$todosContainer = $('#todos-container');
+      this.$todoInput = $('#todo-input');
+      this.getInputValue();
+    },
+  
+    getInputValue: function() {
+      var that = this;
       
-      // Clear input field
-      todoInput.value = '';
-
-      // Append input value
-      var todoHTML = document.createElement('p');
-      todoHTML.innerHTML = todo;
-      todosContainer.appendChild(todoHTML);
+      this.$todoInput.on('keypress', function(event) {
+        var keyPressed = event.which || event.keyCode;
+  
+        // If "Enter" button is pressed...
+        if (keyPressed === ENTER_KEY) {
+          // Get submitted value from input
+          var todo = this.value;
+          todo = '<div class="todo-item"><p>' + todo + '<span class="delete-icon"><i class="fas fa-times-circle fa-2x"></i></span></p></div>';
+          
+          // Clear input field
+          that.$todoInput.val('');
+  
+          // Append input value
+          that.$todosContainer.append(todo);
+        }
+      })
     }
-};
+  };
+
+  App.init();
+})();
