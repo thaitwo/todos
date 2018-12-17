d5b73e2c811ed705a5fbce8337c2b09c6dd35a98
thaitwo <tmtu90@gmail.com>
2018-12-16 23:48:41 -0800

    Item can be deleted with delete icon. Still buggy.

--- src/index.js
+++ src/index.js
@@ -4,24 +4,22 @@
 
   var App = {
     
-    // On submission, add todo to list
-    // Store todo list
-    // Append each new todo item on screen
-    // Always fetch todos from store and display on screen
     // When user clicks on delete icon, remove todo from list
 
 
     init: function() {
       this.$todosContainer = $('#todos-container');
       this.$todoInput = $('#todo-input');
-      this.clearButton = document.getElementById('clear-button');
+      this.$clearButton = $('#clear-button');
+      this.$deleteIcon;
       this.displayTodosOnLoad();
       this.getInputValue();
       this.storageHasData();
       this.activateClearButton();
+      this.activateDeleteIcon();
     },
   
-
+    // Retrieve input value and add to storage
     getInputValue: function() {
       var that = this;
       
@@ -32,31 +30,58 @@
         // Check if "Enter" button is pressed
         if (keyPressed === ENTER_KEY) {
           // Get submitted value from input
-          var todo = this.value;
+          var todoItem = this.value;
           // Add new submitted todo to storage
-          storage.push(todo);
+          var todoList = store.get('todos') || [];
+          todoList.push(todoItem);
           // Once new todo is added to array, add that array back to storage
-          store.set('todos', storage);
+          store.set('todos', todoList);
           // Clear input field
           that.$todoInput.val('');
           // Make todo an HTML element
-          todoHTML = '<div class="todo-item"><p>' + todo + '<span class="delete-icon"><i class="fas fa-times fa-lg"></i></span></p></div>';
+          todoHTML = '<div class="todo-item"><p>' + todoItem + '</p><span class="delete-icon"><i class="fas fa-times fa-lg"></i></span></div>';
           // Append input value
           that.$todosContainer.append(todoHTML);
+          that.$deleteIcon = $('.delete-icon');
           // Make "Clear All" button visible
-          that.clearButton.classList.add('is-visible');
+          that.$clearButton.addClass('is-visible');
         }
       })
     },
 
+
     activateClearButton: function() {
       var that = this;
 
-      this.clearButton.addEventListener('click', function(event) {
+      this.$clearButton.on('click', function(event) {
         event.preventDefault();
 
         store.set('todos', []);
         that.$todosContainer.empty();
+      })
+    },
+
+
+    activateDeleteIcon: function() {
+      var that = this;
+
+      this.$deleteIcon.on('click', function(event) {
+        event.preventDefault();
+        console.log('event', event);
+        // Get name of todo item
+        var todo = event.currentTarget.parentElement.textContent;
+        // Get todos array for store
+        var todoList = store.get('todos');
+        var index = todoList.indexOf(todo);
+
+        if (index > -1) {
+          todoList.splice(index, 1);
+        }
+
+        store.set('todos', todoList);
+        todoList = store.get('todos');
+        
+        that.displayTodos(todoList);
       })
     },
 
@@ -66,20 +91,18 @@
     },
 
 
-    displayTodos: function(data) {
-      var that = this;
-
+    displayTodos: function(todoList) { 
+      this.$todosContainer.empty();     
       // Display each todo item in storage on screen
-      data.forEach(function(todo) {
-        var todoHTML = '<div class="todo-item"><p>' + todo + '<span class="delete-icon"><i class="fas fa-times fa-lg"></i></span></p></div>';
-        that.$todosContainer.append(todoHTML);
-      })
+      var todos = todoList.map(function(todo) {
+        return '<div class="todo-item"><p>' + todo + '</p><span class="delete-icon"><i class="fas fa-times fa-lg"></i></span></div>';
+      });
+      this.$todosContainer.append(todos);
+      this.$deleteIcon = $('.delete-icon');
     },
 
     
     displayTodosOnLoad: function() {
-      var that = this;
-
       // Check if todos storage had any todo items
       if (this.storageHasData() === true) {
         this.displayTodos(storage);
